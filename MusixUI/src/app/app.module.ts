a98d4feba31c10cb6df3b55d7b33fd83c7e61fd4
Vennkat71 <vennkat71@gmail.com>
2020-11-09 05:17:02 +0000

    header component completed

--- MusixUI/src/app/app.module.ts
+++ MusixUI/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
